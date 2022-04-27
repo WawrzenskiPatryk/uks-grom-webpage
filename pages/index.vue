660ce788cd2561c2c1f54e5a32a3a6c7c4ace166
Patryk Wawrzeński <66744401+WawrzenskiPatryk@users.noreply.github.com>
2022-04-27 20:22:00 +0200

    Ready starting project structure (#1)
    * HeaderHamburger component without color variable styling
    * Layout implementation refactor with /layouts directory

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,14 @@
 <template>
-  <the-layout>
+  <main>
     <p>{{ testText }}</p>
     <nuxt-link to="/subpage">Subpage</nuxt-link>
-    
-    <header-hamburger></header-hamburger>
-  </the-layout>
+  </main>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  layout: 'TheLayout',
   data() {
     return {
       testText: 'Below is a link for subpage',
