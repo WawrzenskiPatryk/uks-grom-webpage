c8cb1bafdfff4139417c6895884a3565c105af95
Patryk Wawrzenski <wawrzenskipatryk@gmail.com>
2022-04-28 00:05:56 +0200

    Global configuration for scss variables with 'style-resources' package

--- layouts/TheLayout.vue
+++ layouts/TheLayout.vue
@@ -6,21 +6,6 @@
 </template>
 
 <style lang="scss">
-$color-primary: #55c57a;
-$primary-color: #0a92e0;
-$secondary-color: #efdb03;
-$main-dark: #35495e;
-$secondary-dark: #252525;
-$main-light: #ffffff;
-
-// Light Theme Colors
-$the-text-color: $main-dark;
-$the-background-color: $main-light;
-
-// Dark Theme Colors
-// $the-text-color: $main-light;
-// $the-background-color: $secondary-dark;
-
 * {
   box-sizing: inherit;
   margin: 0;
@@ -34,11 +19,11 @@ html {
 body {
   font-family: 'Lato', sans-serif;
   font-weight: 400;
-  font-size: 1.6rem;
-  // padding-top: 8rem;
+  font-size: $default-font-size;
   line-height: 1.7;
   box-sizing: border-box;
   color: $the-text-color;
   background-color: $the-background-color;
+  // padding-top: 8rem;
 }
 </style>
