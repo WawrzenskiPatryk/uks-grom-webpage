1033626a85493ea7d9f9ebe411497940da3a5bd4
Patryk Wawrzenski <wawrzenskipatryk@gmail.com>
2022-04-27 15:49:16 +0200

    cleanup and first tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial />
+  <h1>{{ testText }}</h1>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      testText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    }
+  },
 }
 </script>
