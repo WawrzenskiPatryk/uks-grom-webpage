7367f211f4b0e28ff374b385909aa6c9126422f3
Patryk Wawrzenski <wawrzenskipatryk@gmail.com>
2022-04-28 09:19:37 +0200

    Layout global SCSS split into new SCSS files

--- layouts/TheLayout.vue
+++ layouts/TheLayout.vue
@@ -6,24 +6,9 @@
 </template>
 
 <style lang="scss">
-* {
-  box-sizing: inherit;
-  margin: 0;
-  padding: 0;
-}
-
-html {
-  font-size: 62.5%;
-}
-
+// devTools: 320, 375, 425, 768, 1024, 1440
 body {
-  font-family: 'Lato', sans-serif;
-  font-weight: 400;
-  font-size: $default-font-size;
-  line-height: 1.7;
-  box-sizing: border-box;
   color: $the-text-color;
   background-color: $the-background-color;
-  // padding-top: 8rem;
 }
 </style>
