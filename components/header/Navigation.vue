cee1666eda9f2f359f5848b3d156613cc7546549
Patryk Wawrzenski <wawrzenskipatryk@gmail.com>
2022-05-17 17:59:56 +0200

    Border radius on tablet menu

--- components/header/Navigation.vue
+++ components/header/Navigation.vue
@@ -29,9 +29,11 @@ export default {
   background-color: $primary-light;
   height: calc(100vh - $header-height);
   width: 100%;
+  border-radius: none;
 
   @media screen and (min-width: $tablet-min-screen-width) {
     box-shadow: $primary-shadow -1rem 1rem 1rem -1rem;
+    border-radius: 0 0 0 1rem;
     text-align: right;
     padding: 0.5rem 1rem 2rem 4rem;
     height: initial;
@@ -40,6 +42,7 @@ export default {
   @media screen and (min-width: $desktop-min-screen-width) {
     box-shadow: none;
     padding: 0 1rem;
+    border-radius: none;
   }
 
   &__list {
