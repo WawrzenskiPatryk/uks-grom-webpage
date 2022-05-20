8d0a08c41f6796d12cea076ee6c50668f05102a4
Patryk Wawrzenski <wawrzenskipatryk@gmail.com>
2022-05-20 18:40:35 +0200

    Fixed wrong galleries titles

--- pages/galeria.vue
+++ pages/galeria.vue
@@ -16,30 +16,30 @@ export default {
     return {
       galleries: [
         {
-          title: 'Galeria testowa 2020',
-          images: [
-            'jumping_kids_football_fun.jpg',
-            'school_physical_education_football.jpg',
-            'jumping_kids_football_fun.jpg',
-            'school_physical_education_football.jpg',
-            'jumping_kids_football_fun.jpg',
-            'school_physical_education_football.jpg',
-            'school_physical_education_football.jpg',
-          ],
-        },
-        {
           title: 'Galeria testowa 2022',
           images: [
-            'white_red_shirt_kids_football_players.jpg',
+            'jumping_kids_football_fun.jpg',
             'school_physical_education_football.jpg',
             'jumping_kids_football_fun.jpg',
-            'white_red_shirt_kids_football_players.jpg',
             'school_physical_education_football.jpg',
             'jumping_kids_football_fun.jpg',
+            'school_physical_education_football.jpg',
+            'school_physical_education_football.jpg',
           ],
         },
         {
           title: 'Galeria testowa 2021',
+          images: [
+            'white_red_shirt_kids_football_players.jpg',
+            'school_physical_education_football.jpg',
+            'jumping_kids_football_fun.jpg',
+            'white_red_shirt_kids_football_players.jpg',
+            'school_physical_education_football.jpg',
+            'jumping_kids_football_fun.jpg',
+          ],
+        },
+        {
+          title: 'Galeria testowa 2020',
           images: [
             'white_red_shirt_kids_football_players.jpg',
             'jumping_kids_football_fun.jpg',
