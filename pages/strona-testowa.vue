c056beb078747a99e20157c69b9cc0d339282338
Patryk Wawrzenski <wawrzenskipatryk@gmail.com>
2022-05-23 10:36:58 +0200

    Implementation of BaseLink component

--- pages/strona-testowa.vue
+++ pages/strona-testowa.vue
@@ -21,32 +21,23 @@
 
     <section>
       <h3>Pliki do pobrania:</h3>
-      <h4>
-        <a
-          class="download-link"
-          href="/downloads/sample.pdf"
-          target="_blank"
-          download
-        >
-          Pobierz PDF
-        </a>
-      </h4>
-      <h4>
-        <a class="download-link" href="/downloads/sample.docx" download>
+
+      <div>
+        <BaseLink path="/downloads/sample.pdf" download> Pobierz PDF </BaseLink>
+      </div>
+      <div>
+        <BaseLink path="/downloads/sample.docx" download>
           Pobierz DOCX
-        </a>
-      </h4>
-      <h4>
-        <a
-          class="download-link"
-          :href="
-            require('~/assets/images/white_red_shirt_kids_football_players.jpg')
-          "
+        </BaseLink>
+      </div>
+      <div>
+        <BaseLink
+          :path="require('~/assets/images/jumping_kids_football_fun.jpg')"
           download
         >
           Pobierz zdjęcie
-        </a>
-      </h4>
+        </BaseLink>
+      </div>
     </section>
   </main>
 </template>
@@ -56,16 +47,3 @@ export default {
   name: 'TestPage',
 };
 </script>
-
-<style lang="scss" scoped>
-.download-link {
-  color: $primary-color-darken;
-  text-decoration: underline;
-  transition: color 0.3s ease;
-  @media (hover: hover) {
-    &:hover {
-      color: $primary-color-lighten;
-    }
-  }
-}
-</style>
