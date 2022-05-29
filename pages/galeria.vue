3db5f75d484281652ebe4ab278b4bdb5959b23ee
marcin <marcin.nieznaj@gmail.com>
2022-05-29 13:50:55 +0200

    gallery refactor

--- pages/galeria.vue
+++ pages/galeria.vue
@@ -1,8 +1,8 @@
 <template>
-  <main class="gallery">
+  <main v-if="!isLoading" class="gallery">
     <GallerySection
-      v-for="(gallery, index) in galleries"
-      :key="index"
+      v-for="(gallery) in unshiftedGalleries"
+      :key="gallery.title"
       :gallery="gallery"
       class="gallery__section"
     />
@@ -22,44 +22,55 @@ export default {
   data() {
     return {
       galleries: [],
+      galleriesReferences: [],
+      isLoading: false
     };
   },
   // @mnieznaj adviced to use mounted() here, but from my observation
   // created() allows the app to download images significantly faster
-  created() {
-    this.getImagesFromStorage();
+  computed: {
+    unshiftedGalleries() {
+      return [...this.galleries].reverse()
+    }
+  },
+  async created() {
+    this.isLoading = true
+    try {
+      this.galleries = await this.getImagesFromStorage();
+    } finally {
+      this.isLoading = false
+    }
   },
   methods: {
-    getImagesFromStorage() {
+    setGalleryName(name) {
+      return name.replace(/-/g, ' ')
+    },
+    getReferenceItems(reference){
+      return listAll(reference).then(result => result.items)
+    },
+    getReferencePrefixes(reference){
+      return listAll(reference).then(result => result.prefixes)
+    },
+    async getImagesFromStorage() {
       const storage = getStorage();
       const mainFolderName = 'gallery';
       const mainFolderRef = firebaseRef(storage, mainFolderName);
 
-      listAll(mainFolderRef).then(result => {
-        result.prefixes.forEach(folderRef => {
-          this.galleries.unshift({
-            title: folderRef.name.replace(/-/g, ' '),
-            images: [],
-          });
+      const imageGalleries = []
 
-          listAll(folderRef).then(result => {
-            result.items.forEach((itemRef, index) => {
-              this.galleries.forEach(gallery => {
-                const areTitlesMatching = itemRef.fullPath.includes(
-                  gallery.title.replace(/ /g, '-')
-                );
+      this.galleriesReferences = await this.getReferencePrefixes(mainFolderRef)
 
-                if (areTitlesMatching) {
-                  getDownloadURL(itemRef).then(url => {
-                    gallery.images[index] = url;
-                    gallery.images.push();
-                  });
-                }
-              });
-            });
-          });
+      this.galleriesReferences.forEach(async (galleryReference) => {
+        const itemsInGallery = await this.getReferenceItems(galleryReference)
+        const imagesList = await Promise.all(itemsInGallery.map((item) => getDownloadURL(item)))
+        // because it is a promise, sometimes they might be returned in different order
+        imageGalleries.push({
+          title: this.setGalleryName(galleryReference.name),
+          images: [...imagesList]
         });
       });
+
+      return imageGalleries
     },
   },
 };
